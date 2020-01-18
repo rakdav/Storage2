d0a97e0c557c1b6307ef597566c98cc6300b42d2
admin <rakdav@inbox.ru>
2020-01-18 18:23:42 +0200

    first

--- app/src/main/java/com/example/storage2/MainActivity.java
+++ app/src/main/java/com/example/storage2/MainActivity.java
@@ -3,12 +3,35 @@ package com.example.storage2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private EditText text;
+    private Button writeButton;
+    private TextView textReader;
+    private Button readButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        text=findViewById(R.id.save_text);
+        writeButton=findViewById(R.id.saveButton);
+        textReader=findViewById(R.id.text);
+        readButton=findViewById(R.id.buttonRead);
+        writeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        readButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
